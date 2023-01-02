dcda9676157992b4f8d02489dbdb215a81ed4f19
ReginYuan <2496943860@qq.com>
2023-01-02 23:02:33 +0800

    搭建项目

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <!-- <n-button type="primary" size="medium" @click="">帅哥</n-button> -->
+    <button
+      class="
+        !text-purple-600
+        border border-purple-600
+        px-[20px]
+        py-[10px]
+        !rounded-full
+        hover:!text-white hover:!text-purple-600
+      "
+    >
+      帅哥
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+import { NButton } from "naive-ui";
+</script>
+<style>
+</style>
+
