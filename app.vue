37d5a01cd515c580953738232edbeb0c7f841517
ReginYuan <2496943860@qq.com>
2023-01-03 00:01:57 +0800

    自定义全局loading实现

--- app.vue
+++ app.vue
@@ -1,24 +1,8 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <!-- <n-button type="primary" size="medium" @click="">帅哥</n-button> -->
-    <button
-      class="
-        !text-purple-600
-        border border-purple-600
-        px-[20px]
-        py-[10px]
-        !rounded-full
-        hover:!text-white hover:!text-purple-600
-      "
-    >
-      帅哥
-    </button>
-  </div>
+  <NuxtPage />
 </template>
 <script setup lang="ts">
 import { NButton } from "naive-ui";
 </script>
 <style>
 </style>
-
---
+++ pages/test-2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <h1>test-2</h1>
+      <nuxt-link to="/">test</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
