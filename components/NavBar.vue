4f2cdd60d6eed6df548e934f71e89cdb4fefb877
ReginYuan <2496943860@qq.com>
2023-05-04 20:35:00 +0800

    feat: 修改对应数据

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>头部</div>
+  <div class="navbar">
+    <div class="container flex items-center h-[60px]">
+      123
+      <!-- <n-icon size="40">
+        <Search></Search>
+      </n-icon> -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+// import { NIcon } from "naive-ui";
+// import { Search } from "@vicons/ionicons5";
 </script>
 
 <style scoped>
+.navbar {
+  z-index: 1000;
+  /* @apply bg-gray-400 fixed top-0 left-0 right-0 shadow-sm; */
+}
+
 </style>
--- unocss.config.ts
+++ unocss.config.ts
@@ -3,6 +3,7 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
+  transformerDirectives
 } from 'unocss'
 
 // @unocss-include
@@ -19,4 +20,5 @@ export default defineConfig({
       scale: 1.2,
     }),
   ],
+  transformers: [transformerDirectives()],
 })
